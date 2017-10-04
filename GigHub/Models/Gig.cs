cd88809b56ceb5bcb6f01f36a7c99b409d145c96
Praneel Misthry <pmisthry@gmail.com>
2017-10-04 16:33:28 +0200

    Added domain classes and tables

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; } 
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }   
+
+        //Navigation Property
+        public Genre Genre { get; set; }    
+    }
+}
--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GigHub.Models
 {
@@ -20,6 +20,10 @@ namespace GigHub.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        //Add references to domain classes
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }    
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
