7c5ee5d9a6d1ff2e07725e9e3f5a7e5169c56d25
Praneel Misthry <pmisthry@gmail.com>
2017-10-06 15:47:16 +0200

    Create Add a Gig Form

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
+        private readonly ApplicationDbContext _context;
+
+        //Initialze the db context in the constructor
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+ 
         public ActionResult Create()
         {
-            return View();
+            //Get the list of Genres from the db
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList(),
+
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }  
+        
+        //For drop down lists we need the numeric value of each option
+        //This is meant to be the genre Id as stored in the database
+        public int Genre { get; set; }  
+
+        //Hold the list of Genres
+        //This is simpler than a list as we are not going to add anything to it
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
