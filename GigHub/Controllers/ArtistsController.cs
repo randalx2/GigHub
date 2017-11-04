f08d43e4dec673995d22b21880cd147b21bff630
Praneel Misthry <pmisthry@gmail.com>
2017-11-04 23:59:56 +0200

    Showing artist I'm following

---
+++ GigHub/Controllers/ArtistsController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Artists
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var artists = _context.Followings
+                .Where(u => u.FollowerId == userId)
+                .Select(a => a.Artist)
+                .ToList();
+
+            return View(artists);
+        }
+
+    }
+}
